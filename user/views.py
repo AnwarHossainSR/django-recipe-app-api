02eb29420e789711f7cde9de43968ad6dcc4bf1f
Anwar Hossain <mahedianwar@gmail.com>
2023-11-05 00:16:10 +0600

    added authentication with jwt

--- user/views.py
+++ user/views.py
@@ -1,20 +1,43 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from user.renderers import UserRenderer
 
 from user.utils import get_tokens_for_user
-from user.serializers import  UserRegistrationSerializer
-from django.contrib.auth import get_user_model
+from user.serializers import  UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+from django.contrib.auth import get_user_model,authenticate
+from rest_framework.permissions import IsAuthenticated
 
 
 User = get_user_model()
 
 
 class UserRegistrationView(APIView):
+  renderer_classes = [UserRenderer]
   def post(self, request, format=None):
-    print(request.data)
     serializer = UserRegistrationSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     user = serializer.save()
     token = get_tokens_for_user(user)
     return Response({'data':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+class UserLoginView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserLoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.data.get('email')
+    password = serializer.data.get('password')
+    user = authenticate(email=email, password=password)
+    if user is not None:
+      token = get_tokens_for_user(user)
+      return Response({'data':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
+    else:
+      return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+    
+class UserProfileView(APIView):
+  renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def get(self, request, format=None):
+    serializer = UserProfileSerializer(request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
